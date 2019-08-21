3813762602ff8cdaa87686a5fae2d2ad6472dbdd
Ivan Grushevich <githubmail911@gmail.com>
2019-08-22 01:47:50 +0300

    Add footer to bottom of page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,9 +8,10 @@ import { AppComponent } from './app.component';
 import { PageComponent } from './page/page.component';
 import { MaterialModule } from './material/material.module';
 import { HeaderComponent } from './page/navigation/header/header.component';
+import { FooterComponent } from './page/navigation/footer/footer.component';
 
 @NgModule({
-  declarations: [AppComponent, PageComponent, HeaderComponent],
+  declarations: [AppComponent, PageComponent, HeaderComponent, FooterComponent],
   imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule, MaterialModule],
   bootstrap: [AppComponent]
 })
