b6471dfb55aebf71d2c3df39a1dd2443762a2fc3
Ivan Grushevich <githubemail911@gmail.com>
2019-08-22 02:44:50 +0300

    Improve routing, add empty page components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,23 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { PageComponent } from './page/page.component';
+import { ContactsComponent } from './contacts/contacts.component';
+import { LicencesComponent } from './licences/licences.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { AboutComponent } from './about/about.component';
 
 const routes: Routes = [
   { path: 'not-found', loadChildren: () => import('./not-found/not-found.module').then(mod => mod.NotFoundModule) },
-  { path: '', component: PageComponent },
+  {
+    path: '',
+    component: PageComponent,
+    children: [
+      { path: 'about', component: AboutComponent },
+      { path: 'projects', component: ProjectsComponent },
+      { path: 'licences', component: LicencesComponent },
+      { path: 'contacts', component: ContactsComponent }
+    ]
+  },
   { path: '**', redirectTo: 'not-found' }
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,9 +9,13 @@ import { PageComponent } from './page/page.component';
 import { MaterialModule } from './material/material.module';
 import { HeaderComponent } from './page/navigation/header/header.component';
 import { FooterComponent } from './page/navigation/footer/footer.component';
+import { AboutComponent } from './about/about.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { LicencesComponent } from './licences/licences.component';
+import { ContactsComponent } from './contacts/contacts.component';
 
 @NgModule({
-  declarations: [AppComponent, PageComponent, HeaderComponent, FooterComponent],
+  declarations: [AppComponent, PageComponent, HeaderComponent, FooterComponent, AboutComponent, ProjectsComponent, LicencesComponent, ContactsComponent],
   imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule, MaterialModule],
   bootstrap: [AppComponent]
 })
