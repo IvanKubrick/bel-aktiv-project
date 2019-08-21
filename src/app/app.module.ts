9cdc3b6639005a1b3f349b01f75c1770ac17b909
Ivan Grushevich <githubmail911@gmail.com>
2019-08-22 01:27:10 +0300

    Add angular material, create basic header component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,17 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PageComponent } from './page/page.component';
+import { MaterialModule } from './material/material.module';
+import { HeaderComponent } from './page/navigation/header/header.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    PageComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
+  declarations: [AppComponent, PageComponent, HeaderComponent],
+  imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule, MaterialModule],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
