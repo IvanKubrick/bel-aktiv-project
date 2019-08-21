e431510d15616a7863a3dccd3f0512655e948734
Ivan Grushevich <githubmail911@gmail.com>
2019-08-22 00:32:29 +0300

    Add basic routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { PageComponent } from './page/page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'not-found', loadChildren: () => import('./not-found/not-found.module').then(mod => mod.NotFoundModule) },
+  { path: '', component: PageComponent },
+  { path: '**', redirectTo: 'not-found' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
