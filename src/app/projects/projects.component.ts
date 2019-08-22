0af47e134e451712593aa066ccc784aa4212f92b
Ivan Grushevich <githubemail911@gmail.com>
2019-08-22 08:46:32 +0300

    Add images, update project cards

--- src/app/projects/projects.component.ts
+++ src/app/projects/projects.component.ts
@@ -11,33 +11,33 @@ export class ProjectsComponent {
   public projects: Project[] = [
     {
       title: 'Реконструкция здания под специализированное на розничной торговле по пер. Корженевского, 20а в г. Минске',
-      imgSrc: '11111'
+      imgSrc: 'assets/img/projects/project-1.jpg'
     },
     {
       title: 'Навес по ул. Павлова, 5а в г. Могилеве с благоустройством территории',
-      imgSrc: '11111'
+      imgSrc: 'assets/img/projects/project-2.jpg'
     },
     {
       title: 'Строительство магазина взамен сносимого здания магазина №2 по адресу: г. Заславль, ул. Путейко',
-      imgSrc: '11111'
+      imgSrc: 'assets/img/projects/project-3.jpg'
     },
     {
       title: 'Строительство объекта торговли в а / г Лошаны Минского района',
-      imgSrc: '11111'
+      imgSrc: 'assets/img/projects/project-4.jpg'
     },
     {
       title: 'Строительство производственной базы в г. Фаниполь, Дзержинского района',
-      imgSrc: '11111'
+      imgSrc: 'assets/img/projects/project-5.jpg'
     },
     {
       title: `Реконструкция производственно-складского комплекса, расположенного по адресу: Минский район,
         Щомыслицкий с/с, 58, район д. Новый Двор`,
-      imgSrc: '11111'
+      imgSrc: 'assets/img/projects/project-6.jpg'
     },
     {
       title:
         'Реконструкция здания бывшей военной комендатуры по переулку Водопроводному, 11 в г. Слониме под жилой дом',
-      imgSrc: '11111'
+      imgSrc: 'assets/img/projects/project-7.jpg'
     }
   ];
 
